8fc6f8faa4a21430af05fec9017fa998462f306d
Yash Varshney <107148830+varshney-yash@users.noreply.github.com>
2022-06-29 23:47:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true});
+const admin = require('firebase-admin');
+const serviceAccount = require("./chat-bot-996a9-firebase-adminsdk-t70q7-58d6f8550f.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:"https://chat-bot-996a9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+});
+
+// import the sessions client from dialogflow
+const { SessionsClient } = require('dialogflow');
+
+// setup http cloud function
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const { queryInput, sessionId } = request.body;
+
+
+    const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+    const session = sessionClient.sessionPath('your-project', sessionId);
+
+
+    const responses = await sessionClient.detectIntent({ session, queryInput});
+
+    const result = responses[0].queryResult;
+
+    response.send(result);
+  });
+});
